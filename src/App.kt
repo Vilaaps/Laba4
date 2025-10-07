23966208dd34e872060119ad9851ebe1d449e22b
Olesya <lesa76632@gmail.com>
2025-10-07 17:34:53 +0300

    Initial commit

---
+++ src/App.kt
@@ -0,0 +1,67 @@
+﻿//fun main() {
+//    greetPlayer()
+//    greetPlayer()
+//    showmessage("sdafsf")
+//    showplayerstats("Арагон",10000)
+//    createCharacter("Арагон",10,"рыцарь")
+//    createCharacter("Арагон",10,)
+//    createCharacter("Арагон",)
+//    createItem("Меч",50)
+//    createItem(price = 200, name = "Зелья")
+//    createItem(price = 150, name = "посох", isMagic = true)
+//    val sword = mutableListOf<String>("Меч")
+//    upgargeWeapon(sword)
+//    println(sword)
+//    val damage = calculateDamage(50,1.5)
+//    println(damage)
+//    showWarning("Низкий уровень здоровья")
+//    println(isPlayerAlive(10))
+//    println(isPlayerAlive(0))
+//    val a = sum(1,2)
+//    val b = sum(1/5,2.5)
+//    val c = sum(1,2,3)
+//    val d = sum(2,1.5)
+//    val e = sum(1.5,2)
+//    println(a)
+//    println(b)
+//    println(c)
+//    println(d)
+//    println(e)
+//    atteck()
+//    attack("Гоблин")
+//    attack("дракон",50)
+//    attack("ork")
+//    attack(1001)
+//    usePotion("Лечебное",50)
+//    usePotion(30,"Магическое")
+//    greet("Игрок")
+//    val add = fun(x: Int, y: Int): Int = x + y
+//    val plus = fun(a: Int, b: Int): Int = a + b
+//    println(add(10, 15))
+//    println(plus(20, 5))
+//    println(
+//        fun(a:Int,b:Int): Int{
+//        return a+b
+//    }(10,15))
+//    val hello = {println("Hello kotlin")}
+//    hello()
+//    val hello: ()->Unit = {println("Hello kotlin")}
+//    attack("Гобдлин")
+//    println("Урон: ${calculateDamage(10,1.5)}")
+//    val numbers = listOf<Int>(1,2,3)
+//    val doubled = numbers.map{it *2}
+//    println(doubled)
+//    val numbers = listOf<Int>(1,2,3,4,34,56,67,89,10,23)
+//    val doubled = numbers.map{it % 2 ==0}
+//    println(doubled)
+//    val numbers = listOf<Int>(1, 2, 3, 4, 34, 56, 67, 89, 10, 23)
+//    val doubled = numbers.filter{ it % 2 == 0 }
+//    println(doubled)
+//    applyEffect { it + 20  }
+//    applyEffect { it- 50  }
+//    onevent("Дождь"){
+//        println("Игрок получает эффект 'мокрый'")
+//    }
+//    val kills = createCounter()
+//    println(kills())
+//    println(kills())
